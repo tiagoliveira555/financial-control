0263bd969564045bd8d69e08ca9a31c5b452334b
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-04 16:00:41 -0300

    refactor

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    server: {
-        hmr: {
-            host: 'https://financial-control-production-ba3a.up.railway.app',
-        }
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
