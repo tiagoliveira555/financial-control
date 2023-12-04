f617028d6de8c8ad852698697d8b1b64e3b2fa87
Tiago Oliveira <tiagoliveira555@gmail.com>
2023-12-04 15:41:33 -0300

    update css and css in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Controle Financeiro</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="bg-gray-200">
         <livewire:home />
